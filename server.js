9770e68578ab3de69a15935525483141cfb17c8c
Aakash Shigwan <vynce.founders@gmail.com>
2026-04-06 01:23:01 +0530

    Backend basic setup

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+  })
+  .catch(err => console.log(err));
