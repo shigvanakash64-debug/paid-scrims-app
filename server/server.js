115daa72854b527d7bc3d73d7ebdbf4cdad90704
Aakash Shigwan <vynce.founders@gmail.com>
2026-06-05 20:59:35 +0530

    51

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-dotenv.config({ path: './.env' });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 import express from "express";
 import mongoose from "mongoose";
